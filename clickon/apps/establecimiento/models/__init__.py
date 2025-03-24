d6d843af7cfc431c21ec7a87902233e6ca48a8b1
Junior Renteria <106351834+Jrenteria19@users.noreply.github.com>
2025-03-23 18:22:33 -0700

    Delete clickon directory

--- clickon/apps/establecimiento/models/__init__.py
+++
@@ -1,8 +0,0 @@
-from .establecimiento import Establecimiento
-from .horario import Horario, Dia
-
-__all__ = [
-    'Establecimiento',
-    'Horario',
-    'Dia'
-]
--- clickon/apps/ordenes/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from .orden import Orden
-from .orden_detalles import ProductosOrden
-
-__all__ = [
-    'Orden',
-    'ProductosOrden'
-]
